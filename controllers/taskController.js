5923a1805439ca5a0d38f279eb11ce455428e466
hamza <sahamzashah19@gmail.com>
2024-08-27 17:40:26 +0500

    update

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -31,11 +31,11 @@ const updateTodo = async (req, res) => {
   try {
     const id = req.params.id;
     const { title, description } = req.body;
-    const task = await Todo.findOne({ _id: id });
+    const task = await Todo.findById(id);
     if (task) {
       if (task.creator == req.userId) {
-        const updatedTask = await Todo.findOneAndUpdate(
-          { id },
+        const updatedTask = await Todo.findByIdAndUpdate(
+          id,
           {
             $set: {
               title,
@@ -58,8 +58,77 @@ const updateTodo = async (req, res) => {
     res.status(500).send({ message: "Some error occurred" });
   }
 };
+const deleteTodo = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const id = req.params.id;
+    const task = await Todo.findById(id);
+    if (!task) {
+      return res.status(404).send({ message: "Task not found" });
+    } else {
+      if (task.creator == req.userId) {
+        await Todo.findByIdAndDelete(id);
+        res.status(204).send({ message: "Deleted Successfully!" });
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: "Some error occurred" });
+  }
+};
+
+const getTodos = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const { title, description } = req.query;
+
+  // Sorting criteria
+  const sortField = req.query.sortBy || "createdAt";
+  const sortOrder = req.query.order === "desc" ? -1 : 1;
+
+  try {
+    let filter = { creator: req.userId };
+    if (title) {
+      filter.title = { $regex: title, $options: "i" };
+    }
+    if (description) {
+      filter.description = { $regex: description, $options: "i" };
+    }
+
+    const total = await Todo.countDocuments(filter);
+    const todos = await Todo.find(filter)
+      .sort({ [sortField]: sortOrder })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .select("title description createdAt");
+
+    res.status(200).json({
+      data: todos,
+      page,
+      limit,
+      total,
+      sortOrder,
+      sortField,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: "Some error occurred" });
+  }
+};
+
+module.exports = {
+  getTodos,
+};
 
 module.exports = {
   createTodo,
   updateTodo,
+  deleteTodo,
+  getTodos,
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -15,4 +15,17 @@ router.post(
   todoController.createTodo
 );
 
+router.get("/", verifyToken, todoController.getTodos);
+
+router.put(
+  "/:id",
+  [
+    body("title", "Title cannot be null").exists(),
+    body("description", "Description cannot be null").exists(),
+  ],
+  verifyToken,
+  todoController.updateTodo
+);
+router.delete("/:id", verifyToken, todoController.deleteTodo);
+
 module.exports = router;
